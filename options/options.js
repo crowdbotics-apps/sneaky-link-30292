2a51b793bf682dcc56044299b9b899e393e9daa1
Crowdbotics <team@crowdbotics.com>
2021-09-03 06:41:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "sneaky_link_30292Identifier",
+  url: "https://sneaky_link_30292Identifier.botics.co",
+  api: "https://sneaky_link_30292Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
