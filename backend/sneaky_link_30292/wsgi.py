638053d1e61ee3742653d0882ef358725b3296d5
Crowdbotics <team@crowdbotics.com>
2021-09-03 06:43:07 +0000

    Update models

--- backend/sneaky_link_30292/wsgi.py
+++ backend/sneaky_link_30292/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sneaky_link_30292.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sneaky_link_30292.settings")
 
 application = get_wsgi_application()
